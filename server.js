28e53ef77a7745280949ecbe4d1f9a0ce8592d71
Risky <kiki014045@gmail.com>
2020-02-27 05:11:40 +0700

    Heroku

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 
 const express = require("express");
+var cors = require('cors')
+
 const app = express();
 
 const routes = require("./routes");
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000 ;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/v1", routes);
 
